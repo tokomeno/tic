81eebd2dc2bf7c50edc55c92ba54c5dd49f27505
tokomeno <tokamenabde@gmail.com>
2019-01-18 19:45:28 +0400

    init

--- src/App.js
+++ src/App.js
@@ -5,24 +5,87 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Game />
+    );
+  }
+}
+
+class Square extends React.Component {
+ 
+  render() {
+    return (
+      <button 
+        onClick={ ()=> this.props.onClick() }
+        className="square"
+        >
+          {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null)
+    }
+    console.log(this.state)
+  }
+
+  handleClick = i => {
+    const squares = this.state.squares.slice()
+    squares[i]
+  }
+
+  renderSquare(i) {
+    return <Square 
+      value={this.state.squares[i]} 
+      onClick={ () => this.handleClick(i)}
+    />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
       </div>
     );
   }
 }
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+ 
 
 export default App;
